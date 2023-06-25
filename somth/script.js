a30537667309f6ad4ed405eb992366569d21f59c
Temesgen <130209495+TemesgenGetye@users.noreply.github.com>
2023-06-26 02:02:20 +0300

    Add files via upload

---
+++ somth/script.js
@@ -0,0 +1,7 @@
+function navbar(){
+  let expanded=document.querySelector(".container");
+  expanded.classList.toggle("expand")
+  let nav=document.querySelector(".hidden-nav");
+  nav.classList.toggle("hide")
+
+}
